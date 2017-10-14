e739673fb9b376ec44a49fceabce732f9ee878c4
BK <bkam03@yahoo.com>
2017-10-13 16:57:45 -1000

    added route for nlp processing from app in server

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
-const db = require('../models');
+const db = require('./models');
+const nlpRoute = require( './routes/nlp-route.js' );
+
 const Entries = db.entries;
 const Keywords = db.keywords;
 const Users = db.users;
@@ -10,6 +12,7 @@ const Users = db.users;
 const PORT = process.env.PORT || 3001;
 
 app.use('/server', require('./watson/speechToTextAPI.js'));
+app.use( '/entry/new', nlpRoute );
 
 const server = app.listen(PORT, () => {
   db.sequelize.sync({force: true});
