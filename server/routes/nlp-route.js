4c62c46fab69d47c20ff72e25d57f4983dd08b10
BK <bkam03@yahoo.com>
2017-10-13 20:25:06 -1000

    route creates record for entry after receiving watson api response.

--- server/routes/nlp-route.js
+++ server/routes/nlp-route.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const router = express.Router();
+const watson = require(  '../natural-lang-processing/nlpAPI.js' );
 
 const db = require('../models');
+
+const router = express.Router();
+
 const Entries = db.entries;
 const Keywords = db.keywords;
 const Users = db.users;
 
 router.post( '/', ( req, res ) => {
   let textEntry = req.body.text;
+  let user_id = req.body.userId;
+  let entryType = req.body.type;
   //receive json containing finalized text
   console.log( textEntry );
 
@@ -18,23 +23,49 @@ router.post( '/', ( req, res ) => {
   }*/
 
   //send to nlp
+
   watson.analyze( textEntry )
   .then( ( data ) => {
-    console.log( data );
+    let nlpData = JSON.parse( data );
+  //receive nlp data
+    console.log( nlpData );
+  //create db record of entry
+    let sentimentData = nlpData.sentiment.document;
+    let emotionData = nlpData.emotion.document.emotion;
+    Entries.create( {
+      user_id: null, //change later to user_id
+      text: textEntry,
+      sentimentScore: sentimentData.score,
+      sentimentLabel: sentimentData.label,
+      sadnessScore: emotionData.sadness,
+      fearScore: emotionData.fear,
+      angerScore: emotionData.anger,
+      joyScore: emotionData.joy,
+      disgustScore: emotionData.disgust,
+      type: entryType,
+      source_id: null
+
+    } )
+    .then( ( entry ) => {
+      console.log( entry );
+      //create entry of keywords.
+
+      //query database for entry( this entry ) + keywords
+
+      //return entry as json.
+
+
+      res.send();
+    } )
+    .catch( ( err ) => {
+      console.log( err );
+      res.send( err );
+    } );
   } )
   .catch( ( err ) => {
     console.log( err );
+    res.send( err );
   } );
-  //receive nlp data
-
-  //create db record of entry
-
-  //create entry of keywords.
-
-  //query database for entry( this entry ) + keywords
-
-  //return entry as json.
-  res.end();
 } );
 
 
