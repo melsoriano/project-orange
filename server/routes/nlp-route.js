8b24a8a4762a4d8fae8a07e987b6dc4d4568f245
BK <bkam03@yahoo.com>
2017-10-13 17:24:32 -1000

    route receives post json, displays.  added nlp analyze function, untested.

--- server/routes/nlp-route.js
+++ server/routes/nlp-route.js
@@ -6,8 +6,35 @@ const Entries = db.entries;
 const Keywords = db.keywords;
 const Users = db.users;
 
-router.get( '/', ( req, res ) => {
-  console.log( 'hi' );
+router.post( '/', ( req, res ) => {
+  let textEntry = req.body.text;
+  //receive json containing finalized text
+  console.log( textEntry );
+
+  //validate input somehow.
+/*  if( typeof textEntry !== "string" ){
+    console.log( 'invalid input' );
+    res.end();
+  }*/
+
+  //send to nlp
+  watson.analyze( textEntry )
+  .then( ( data ) => {
+    console.log( data );
+  } )
+  .catch( ( err ) => {
+    console.log( err );
+  } );
+  //receive nlp data
+
+  //create db record of entry
+
+  //create entry of keywords.
+
+  //query database for entry( this entry ) + keywords
+
+  //return entry as json.
+  res.end();
 } );
 
 
