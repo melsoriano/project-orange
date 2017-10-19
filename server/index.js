8a3041ef33ff365c97d8745947e245b18e6e66ad
Mellanie Soriano <msoriano808@gmail.com>
2017-10-18 20:40:38 -1000

    integrated passport

--- server/index.js
+++ server/index.js
@@ -1,20 +1,84 @@
 const express = require('express');
 const bp = require( 'body-parser' );
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
 
 const db = require('./models');
 const nlpRoute = require( './routes/nlp-route.js' );
+const userRoute = require( './routes/user-routes.js' );
+const CONFIG = require('./config/config.json');
 
-const Entries = db.entries;
-const Keywords = db.keywords;
-const Users = db.users;
+const Entry = db.entries;
+const Keyword = db.keywords;
+const User = db.users;
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use( bp.json( { extended: true } ) );
+app.use(bp.urlencoded());
 
-app.use('/server', require('./watson/speechToTextAPI.js'));
+app.use(session({
+  store: new RedisStore(),
+  secret: CONFIG.SESSION_SECRET,
+  name: 'orange_sessions',
+  cookie: {
+    maxAge: 10000000
+  }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(
+  (username, password, done) => {
+    User.findOne({
+      where: {
+        username: username
+      }
+    }).then(user => {
+      if (user !== null) {
+        bcrypt.compare(password, user.password).then(result => {
+          if (result) {
+            return done(null, user);
+          } else {
+            return done(null, false, { message: 'username or password incorrect!' });
+          }
+        }).catch(err => console.log(err));
+      } else {
+        throw 'user not found!';
+      }
+    }).catch(err => {
+      return done(null, false, { message: 'username or password incorrect!'});
+    });
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((userId, done) => {
+  User.find({
+    where: {
+      id: userId
+    }
+  }).then(user => {
+    return done(null, {
+      id: user.id,
+      username: user.username
+    });
+  }).catch(err => {
+    done(err, user);
+  });
+});
+
+app.use('/recording', require('./watson/speechToTextAPI.js'));
 app.use( '/entry/new', nlpRoute );
+app.use( '/', userRoute );
 
 const server = app.listen(PORT, () => {
   db.sequelize.sync({force: true});
