4a4a713fc38af3049d6ea3b9715c742622ba6dec
BK <bkam03@yahoo.com>
2017-10-19 20:17:02 -1000

    moved functions out to another file, required in as module to use

--- server/routes/entriesRoutes.js
+++ server/routes/entriesRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const db = require('../models');
+const helperFn = require( './helperFunctions/entriesRoutesHelper.js' );
 
 const router = express.Router();
 
@@ -27,140 +28,10 @@ router.get( '/all', ( req, res ) => {
     } );
 } );
 
-function isDuplicateKeyword( keyword, keywordArray ){
-  let index = -1;
-  for( var i = 0; i < keywordArray.length; i++ ){
-    let keywordFromArray = keywordArray[ i ].keyword;
-    if( keyword ===  keywordFromArray ){
-      index = i;
-      break;
-    }
-  }
-  return index;
-}
-
-function sumKeywordValues( newKeyword, storedKeyword ){
-  let combinedKeyword = {
-    keyword: storedKeyword.keyword,
-    sentimentScore: newKeyword.sentimentScore + storedKeyword.sentimentScore,
-    relevanceScore: newKeyword.relevanceScore + storedKeyword.relevanceScore,
-    sadnessScore: newKeyword.sadnessScore + storedKeyword.sadnessScore,
-    fearScore: newKeyword.fearScore + storedKeyword.fearScore,
-    angerScore: newKeyword.angerScore + storedKeyword.angerScore,
-    joyScore: newKeyword.joyScore + storedKeyword.joyScore,
-    disgustScore: newKeyword.disgustScore + storedKeyword.disgustScore,
-    frequency: storedKeyword.frequency + 1
-
-  };
-  return combinedKeyword;
-}
-
-function combineKeywordsIntoAverage( keywordArray ){
-  let arrayOfKeywordSums = [];
-
-  keywordArray.forEach( ( keywordObj, index ) => {
-    let keywordData = keywordObj.dataValues;
-    let indexOfKeyword = isDuplicateKeyword( keywordData.keyword, arrayOfKeywordSums );
-
-    if( indexOfKeyword === -1 ){
-      let newKeyword = Object.assign( keywordObj.dataValues );
-      newKeyword.frequency = 1;
-      arrayOfKeywordSums.push( newKeyword );
-
-    } else {
-      let keywordBeingStored = arrayOfKeywordSums[ indexOfKeyword ];
-      let combinedKeyword = sumKeywordValues( keywordObj, keywordBeingStored );
-      arrayOfKeywordSums[ indexOfKeyword ] = combinedKeyword;
-    }
-  } );
-  let summaryOfKeywords = arrayOfKeywordSums.map( (  keywordObj ) => {
-    return {
-      keyword: keywordObj.keyword,
-      sentimentScore: keywordObj.sentimentScore / keywordObj.frequency,
-      relevanceScore: keywordObj.relevanceScore,
-      sadnessScore: keywordObj.sadnessScore / keywordObj.frequency,
-      fearScore: keywordObj.fearScore / keywordObj.frequency,
-      angerScore: keywordObj.angerScore / keywordObj.frequency,
-      joyScore: keywordObj.joyScore / keywordObj.frequency,
-      disgustScore: keywordObj.disgustScore / keywordObj.frequency,
-      frequency: keywordObj.frequency
-    };
-  } );
-  return summaryOfKeywords;
-}
-
-function quickSortKeywordsByRelevance ( array ){
-  if( array.length < 2 ){
-    return array;
-  }
-  var pivot = array.shift();
-  var lesserArray = [];
-  var greaterArray = [];
-
-  var currentNumber = null;
-
-  for( var i = 0; i < array.length; i++ ) {
-    currentNumber = array[ i ];
-    ( currentNumber.relevanceScore < pivot.relevanceScore ) ? lesserArray.unshift( currentNumber ) : greaterArray.unshift( currentNumber );
-  }
-  return quickSortKeywordsByRelevance( greaterArray ).concat( pivot, quickSortKeywordsByRelevance( lesserArray ) );
-}
-
-function getTopKeywords( listLength, keywordArray ){
-  let sortedArray = quickSortKeywordsByRelevance( keywordArray );
-  let arrayCutToSpecifiedLength = sortedArray.slice( 0, listLength );
-  return arrayCutToSpecifiedLength;
-}
-
-function getEntriesAndAggregateKeywordsBetweenDates( startDate, endDate, keywordSummaryListLength ){
-  return new Promise( function( resolve, reject ){
-    console.log( `@@@@@@@@@@@@ ${startDate} \n ${endDate}` );
-
-    Entries.findAll( {
-      where : {
-        createdAt: {
-          $between: [ startDate, endDate ]
-        }
-      },
-      include: [
-        {
-          model: Keywords
-        }
-      ]
-    } )
-    .then( ( entries ) => {
-      let returnData = {
-        entries: entries,
-        keywordSummary: null
-      };
-      Keywords.findAll( {
-        where: {
-          createdAt: {
-            $between: [ startDate, endDate ]
-          }
-        }
-      } )
-      .then( ( keywords ) => {
-        let combinedKeywords = combineKeywordsIntoAverage( keywords );
-        let keywordSummary = getTopKeywords( keywordSummaryListLength, combinedKeywords );
-        console.log( keywordSummary );
-        returnData.keywordSummary = keywordSummary;
-        resolve( returnData );
-      } )
-      .catch( ( err ) => {
-        resolve( err );
-      } );
-      } )
-    .catch( ( err )=> {
-      resolve( err );
-    } );
-  } );
-}
-
 router.get( '/yearly', ( req, res ) => {
   let timespanInDays = 365;
   let keywordSummaryLength = 20;
-  getEntriesAndAggregateKeywordsBetweenDates( timespanInDays, keywordSummaryLength )
+  helperFn.getEntriesAndAggregateKeywordsBetweenDates( timespanInDays, keywordSummaryLength )
   .then( ( data ) => {
     res.send( data );
   } )
@@ -178,7 +49,7 @@ router.get( '/monthly', ( req, res ) => {
   let startDateOfQuery = new Date();
   startDateOfQuery.setDate( startDateOfQuery.getDate() - timespanInDays );
 
-  getEntriesAndAggregateKeywordsBetweenDates( startDateOfQuery, endDateOfQuery, keywordSummaryLength )
+  helperFn.getEntriesAndAggregateKeywordsBetweenDates( startDateOfQuery, endDateOfQuery, keywordSummaryLength )
   .then( ( data ) => {
     res.send( data );
   } )
@@ -196,7 +67,7 @@ router.get( '/weekly', ( req, res ) => {
   let startDateOfQuery = new Date();
   startDateOfQuery.setDate( startDateOfQuery.getDate() - timespanInDays );
 
-  getEntriesAndAggregateKeywordsBetweenDates( startDateOfQuery, endDateOfQuery, keywordSummaryLength )
+  helperFn.getEntriesAndAggregateKeywordsBetweenDates( startDateOfQuery, endDateOfQuery, keywordSummaryLength )
   .then( ( data ) => {
     res.send( data );
   } )
@@ -214,7 +85,7 @@ router.get( '/daily', ( req, res ) => {
   let startDateOfQuery = new Date();
   startDateOfQuery.setDate( startDateOfQuery.getDate() - timespanInDays );
 
-  getEntriesAndAggregateKeywordsBetweenDates( startDateOfQuery, endDateOfQuery, keywordSummaryLength )
+  helperFn.getEntriesAndAggregateKeywordsBetweenDates( startDateOfQuery, endDateOfQuery, keywordSummaryLength )
   .then( ( data ) => {
     res.send( data );
   } )
@@ -233,7 +104,7 @@ router.get( '/month/:mmyyDate', ( req, res ) => {
 
   let startDateOfQuery = new Date( yearOfQuery, monthOfQuery, 1, 0, 0, 0, 0 );
   let endDateOfQuery = new Date( yearOfQuery, monthOfQuery + 1, 1, 0, 0, 0, 0 );
-  getEntriesAndAggregateKeywordsBetweenDates( startDateOfQuery, endDateOfQuery, keywordSummaryLength )
+  helperFn.getEntriesAndAggregateKeywordsBetweenDates( startDateOfQuery, endDateOfQuery, keywordSummaryLength )
   .then( ( data ) => {
     res.send( data );
   } )
