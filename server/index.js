6a922557a1735d080412d6dd707c8cb7fbe4653f
Mellanie Soriano <msoriano808@gmail.com>
2017-10-21 14:13:25 -1000

    commit

--- server/index.js
+++ server/index.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const bp = require( 'body-parser' );
-const bcrypt = require('bcrypt');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const session = require('express-session');
-const RedisStore = require('connect-redis')(session);
+const express = require("express");
+const bp = require("body-parser");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
+const RedisStore = require("connect-redis")(session);
 
-const db = require('./models');
-const nlpRoute = require( './routes/nlp-route.js' );
-const userRoute = require( './routes/user-routes.js' );
-const CONFIG = require('./config/config.json');
-const getEntriesRoutes = require( './routes/entriesRoutes.js' );
-const getEntryRoutes = require( './routes/entryRoutes.js' );
-const oauthRoute = require('./routes/oauth.js');
+const db = require("./models");
+const nlpRoute = require("./routes/nlp-route.js");
+const userRoute = require("./routes/user-routes.js");
+const CONFIG = require("./config/config.json");
+const getEntriesRoutes = require("./routes/entriesRoutes.js");
+const getEntryRoutes = require("./routes/entryRoutes.js");
+const oauthRoute = require("./routes/oauth.js");
 
 const Entry = db.entries;
 const Keyword = db.keywords;
@@ -21,45 +21,55 @@ const User = db.users;
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
-app.use( bp.json() );
+app.use(bp.json());
 app.use(bp.urlencoded());
 
-app.use(session({
-  store: new RedisStore(),
-  secret: CONFIG.SESSION_SECRET,
-  name: 'orange_sessions',
-  cookie: {
-    maxAge: 10000000
-  }
-}));
+app.use(
+  session({
+    store: new RedisStore(),
+    secret: CONFIG.SESSION_SECRET,
+    name: "orange_sessions",
+    cookie: {
+      maxAge: 10000000
+    }
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.use(new LocalStrategy(
-  (username, password, done) => {
+passport.use(
+  new LocalStrategy((username, password, done) => {
     User.findOne({
       where: {
         username: username
       }
-    }).then(user => {
-      if (user !== null) {
-        bcrypt.compare(password, user.password).then(result => {
-          if (result) {
-            return done(null, user);
-          } else {
-            return done(null, false, { message: 'username or password incorrect!' });
-          }
-        }).catch(err => console.log(err));
-      } else {
-        throw 'user not found!';
-      }
-    }).catch(err => {
-      return done(null, false, { message: 'username or password incorrect!'});
-    });
-  }
-));
+    })
+      .then(user => {
+        if (user !== null) {
+          bcrypt
+            .compare(password, user.password)
+            .then(result => {
+              if (result) {
+                return done(null, user);
+              } else {
+                return done(null, false, {
+                  message: "username or password incorrect!"
+                });
+              }
+            })
+            .catch(err => console.log(err));
+        } else {
+          throw "user not found!";
+        }
+      })
+      .catch(err => {
+        return done(null, false, {
+          message: "username or password incorrect!"
+        });
+      });
+  })
+);
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
@@ -70,25 +80,26 @@ passport.deserializeUser((userId, done) => {
     where: {
       id: userId
     }
-  }).then(user => {
-    return done(null, {
-      id: user.id,
-      username: user.username
+  })
+    .then(user => {
+      return done(null, {
+        id: user.id,
+        username: user.username
+      });
+    })
+    .catch(err => {
+      done(err, user);
     });
-  }).catch(err => {
-    done(err, user);
-  });
 });
 
-app.use( '/user/entry/new', nlpRoute );
-app.use( '/user/entries', getEntriesRoutes );
-app.use( '/user/entry', getEntryRoutes );
+app.use("/user/entry/new", nlpRoute);
+app.use("/user/entries", getEntriesRoutes);
+app.use("/user/entry", getEntryRoutes);
 
-app.use('/recording', require('./watson/speechToTextAPI.js'));
-app.use( '/entry/new', nlpRoute );
-app.use( '/', userRoute );
-app.use( '/sessions', oauthRoute );
-
+app.use("/recording", require("./watson/speechToTextAPI.js"));
+app.use("/entry/new", nlpRoute);
+app.use("/", userRoute);
+app.use("/twitterauth", oauthRoute);
 
 const server = app.listen(PORT, () => {
   db.sequelize.sync();
