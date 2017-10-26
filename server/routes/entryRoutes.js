15a809e663fd9b14cabbf71a0851f375e0d80f1d
BK <bkam03@yahoo.com>
2017-10-25 20:22:29 -1000

    get route for user/entry/:id only gets entry if from current user

--- server/routes/entryRoutes.js
+++ server/routes/entryRoutes.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('../models');
+const db = require("../models");
 
 const router = express.Router();
 
@@ -8,28 +8,28 @@ const Entries = db.entries;
 const Keywords = db.keywords;
 const Users = db.users;
 
-router.get( '/:id', ( req, res ) => {
+router.get("/:id", (req, res) => {
+  let user_id = req.user.id;
   let entry_id = req.params.id;
-  Entries.findOne( {
+  Entries.findOne({
     where: {
-      id: entry_id
+      id: entry_id,
+      user_id: user_id
     },
     include: [
       {
-        model: Keywords,
+        model: Keywords
       }
     ]
-  } )
-  .then( ( entry ) => {
-    res.send( entry );
-  } )
-  .catch( ( err ) => {
-    res.send( err );
-  } );
-} );
+  })
+    .then(entry => {
+      res.send(entry);
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
 
-router.get( '/:edit', ( req, res ) => {
-
-} );
+router.get("/:edit", (req, res) => {});
 
 module.exports = router;
