e7be955b1ffdd9509b3bfdecaaf3bf2344a58d72
jeffmon <jeffmon@gmail.com>
2017-10-26 14:33:40 -1000

    fixed spacing with text

--- frontend/src/components/Graph/SingleEntry.js
+++ frontend/src/components/Graph/SingleEntry.js
@@ -3,18 +3,37 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class SingleEntry extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
+    if (!this.props.show) {
+      return null;
+    }
     return (
-      <div className="container is-mobile">
-        <div>Single Entry!</div>
+      <div className="modal is-active">
+        <div className="modal-background" onClick={this.props.onHide} />
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <p className="modal-card-title">{this.props.date}</p>
+            <button className="delete" onClick={this.props.onHide} />
+          </header>
+          <section className="modal-card-body">
+            <div className="content">{this.props.entry.text}</div>
+          </section>
+          <footer className="modal-card-foot">
+            <button className="button is-primary" onClick={this.props.onHide}>
+              Close
+            </button>
+          </footer>
+        </div>
       </div>
     );
   }
 }
+
+SingleEntry.propTypes = {
+  closeModal: PropTypes.func,
+  modalState: PropTypes.bool,
+  title: PropTypes.string
+};
 
 const mapStatetoProps = state => {
   return { weekEntries: state.weekEntries };
