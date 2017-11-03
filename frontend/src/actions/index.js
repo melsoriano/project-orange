36882760f79bd1fe61a9acade4a34139f6c5f1a4
Mellanie Soriano <msoriano808@gmail.com>
2017-11-02 18:01:55 -1000

    added react-twitter-auth lib

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,17 +1,17 @@
-import axios from 'axios';
-import querystring from 'querystring';
-import { sessionService } from 'redux-react-session';
-export const LOAD_ENTRIES = 'LOAD_ENTRIES';
-export const ADD_ENTRY = 'ADD_ENTRY';
-export const GET_ONE_ENTRY = 'GET_ONE_ENTRY';
-export const GET_WEEK_ENTRIES = 'GET_WEEK_ENTRIES';
-export const LOAD_AUTH = 'LOAD_AUTH';
-export const LOGIN_USER = 'LOGIN_USER';
-export const GET_MONTH_ENTRIES = 'GET_MONTH_ENTRIES';
+import axios from "axios";
+import querystring from "querystring";
+import { sessionService } from "redux-react-session";
+export const LOAD_ENTRIES = "LOAD_ENTRIES";
+export const ADD_ENTRY = "ADD_ENTRY";
+export const GET_ONE_ENTRY = "GET_ONE_ENTRY";
+export const GET_WEEK_ENTRIES = "GET_WEEK_ENTRIES";
+export const LOAD_AUTH = "LOAD_AUTH";
+export const LOGIN_USER = "LOGIN_USER";
+export const GET_MONTH_ENTRIES = "GET_MONTH_ENTRIES";
 
 export const getMonthEntries = () => {
   return dispatch => {
-    axios.get('/user/entries/monthly').then(entries => {
+    axios.get("/user/entries/monthly").then(entries => {
       dispatch({
         type: GET_MONTH_ENTRIES,
         monthEntries: entries.data
@@ -23,9 +23,9 @@ export const getMonthEntries = () => {
 export const addEntry = entry => {
   return dispatch => {
     axios
-      .post('/user/entry/new', {
+      .post("/user/entry/new", {
         text: entry,
-        type: 'text-entry'
+        type: "text-entry"
       })
       .then(oneEntry => {
         dispatch({
@@ -38,7 +38,7 @@ export const addEntry = entry => {
 
 export const getEntries = () => {
   return dispatch => {
-    axios.get('/user/entries/all').then(entries => {
+    axios.get("/user/entries/all").then(entries => {
       dispatch({
         type: LOAD_ENTRIES,
         entries: entries.data
@@ -49,7 +49,7 @@ export const getEntries = () => {
 
 export const getWeekEntries = () => {
   return dispatch => {
-    axios.get('/user/entries/weekly').then(entries => {
+    axios.get("/user/entries/weekly").then(entries => {
       dispatch({
         type: GET_WEEK_ENTRIES,
         weekEntries: entries.data
@@ -60,7 +60,7 @@ export const getWeekEntries = () => {
 
 export const loginUser = user => {
   return dispatch => {
-    axios.post('/login', querystring.stringify(user)).then(res => {
+    axios.post("/login", querystring.stringify(user)).then(res => {
       const { token } = res;
       console.log(res);
       sessionService.saveSession({ token }).then(() => {
@@ -76,7 +76,7 @@ export const loginUser = user => {
 
 export const checkUser = () => {
   return dispatch => {
-    axios.get('/auth').then(res => {
+    axios.get("/auth").then(res => {
       dispatch({
         type: LOAD_AUTH,
         auth: res.data
