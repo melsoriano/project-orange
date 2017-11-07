5e04220bb96f3133d8fc6fd480fc62f5806e3e2f
BK <bkam03@yahoo.com>
2017-11-06 19:10:06 -1000

    removed line 4 month...

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,10 +1,9 @@
-import { combineReducers } from 'redux';
-import { sessionReducer } from 'redux-react-session';
-import weekEntries from './weeklyEntry_reducer';
-import monthEntries from './monthlyEntry_reducer';
-import userData from './user_reducer';
-import twitterEntries from './twitter_reducer';
-import auth from './auth_reducer';
+import { combineReducers } from "redux";
+import { sessionReducer } from "redux-react-session";
+import weekEntries from "./weeklyEntry_reducer";
+import userData from "./user_reducer";
+import twitterEntries from "./twitter_reducer";
+import auth from "./auth_reducer";
 
 const reducers = combineReducers({
   weekEntries,
