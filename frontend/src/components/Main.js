b7777f8f029047ea5a0cbd8e09165af5337766d8
Mellanie Soriano <msoriano808@gmail.com>
2017-11-06 17:46:33 -1000

    changed to single quotes to fix merge conflict

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
-import { Route } from "react-router-dom";
-import Home from "./Home";
-import Graph from "./Graph/Graph";
-import Settings from "./Settings/Settings";
-import User from "./User/User";
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Home from './Home';
+import Graph from './Graph/Graph';
+import Settings from './Settings/Settings';
+import User from './User/User';
 
 class Main extends Component {
   render() {
