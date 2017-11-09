981c217040987ef5a8b2788a93b4b77e6bde03f3
Seolhee Kim <seolhee@upspringmedia.com>
2017-11-09 11:34:29 -1000

    added the footertab

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import Home from './Home';
-import Graph from './Graph/Graph';
-import Settings from './Settings/Settings';
-
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Home from "./Home";
+import Graph from "./Graph/Graph";
+import Settings from "./Settings/Settings";
+import FooterTab from "./FooterTab";
 
 class Main extends Component {
   render() {
     return (
-      <div className="mainPage_container">
+      <section className="Main">
         <Route exact path="/" component={Home} />
         <Route path="/graph" component={Graph} />
         <Route path="/settings" component={Settings} />
-      </div>
-
+        <div className="FooterTab">
+          <FooterTab />
+        </div>
+      </section>
     );
   }
 }
