8e3bb71cc363c35d2ef23f2d3b4e9d950d0e5a10
Seolhee Kim <seolhee@upspringmedia.com>
2017-11-09 11:15:49 -1000

    changed the html tag to nav

--- frontend/src/components/FooterTab.js
+++ frontend/src/components/FooterTab.js
@@ -1,51 +1,44 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class FooterTab extends Component {
   render() {
     return (
-      <section className="footerTab_container">
-        <div class="hero-foot">
-          <div class="tabs is-fullwidth">
-            <div class="container">
-              <ul>
-                <li>
-                  <Link to="/">
-                    <span className="icon is-medium ">
-                      <i className="fa fa-home " />
-                    </span>
-                  </Link>
-                </li>
-
-                <li>
-                  <Link to="/graph">
-                    <span className="icon is-medium">
-                      <i className="fa fa-area-chart" />
-                    </span>
-                  </Link>
-                </li>
-
-                <li>
-                  <Link to="/settings">
-                    <span className="icon is-medium">
-                      <i className="fa fa-gear" />
-                    </span>
-                  </Link>
-                </li>
-
-                <li>
-                   <Link to="/quotes">
-                    <span className="icon is-medium">
-                      <i className="fa fa-quote-right" />
-                    </span>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
+      <section className="footerNavbar">
+        <nav className="navbar">
+          <ul className="nav_links has-text-centered">
+            <li>
+              <a href="/">
+                <span className="icon is-medium ">
+                  <i className="fa fa-home " />
+                </span>
+              </a>
+            </li>
+            <li>
+              <a href="/graph">
+                <span className="icon is-medium">
+                  <i className="fa fa-area-chart" />
+                </span>
+              </a>
+            </li>
+            <li>
+              <a href="/settings">
+                <span className="icon is-medium">
+                  <i className="fa fa-gear" />
+                </span>
+              </a>
+            </li>
+            <li>
+              <a href="/settings">
+                <span className="icon is-medium">
+                  <i className="fa fa-gear" />
+                </span>
+              </a>
+            </li>
+          </ul>
+        </nav>
       </section>
-    )
+    );
   }
 }
 
