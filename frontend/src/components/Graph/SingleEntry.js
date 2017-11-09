eb48d23e753f7adf90187ada3312315a1e6ce5e5
Seolhee Kim <seolhee@upspringmedia.com>
2017-11-09 11:31:55 -1000

    didn't change anything

--- frontend/src/components/Graph/SingleEntry.js
+++ frontend/src/components/Graph/SingleEntry.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 class SingleEntry extends Component {
   progressColor(e) {
